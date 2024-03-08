6b5ceac4462e0edf6927cc20dbc67630c1f011ed
Derek Grech <djgrech@gmail.com>
2024-03-08 14:55:40 +0100

    updated

--- BenchMark/BenchMark/ExpressionSetterByTypes.cs
+++ BenchMark/BenchMark/ExpressionSetterByTypes.cs
@@ -3,14 +3,21 @@ using System.Reflection;
 
 namespace BenchMark
 {
-    public class CustomExpressionMeta<T>
+    public class CustomExpressionMeta<TEntity>
     {
-        public Dictionary<string, Action<T, string>> stringFuncMap { get; set; }
-        public Dictionary<string, Action<T, int>> intFuncMap { get; set; }
-        public Dictionary<string, Action<T, decimal>> decimalFuncMap { get; set; }
+        public Dictionary<string, Action<TEntity, string>> stringFuncMap { get; set; }
+        public Dictionary<string, Action<TEntity, int>> intFuncMap { get; set; }
+        public Dictionary<string, Action<TEntity, decimal>> decimalFuncMap { get; set; }
     }
 
-    public class CustomExpressionSetter<TEntity>
+    public interface ICustomExpressionSetter<in TEntity>
+    {
+        void Set(TEntity instance, string propertyName, string value);
+        void Set(TEntity instance, string propertyName, decimal value);
+        void Set(TEntity instance, string propertyName, int value);
+    }
+
+    public class CustomExpressionSetter<TEntity> : ICustomExpressionSetter<TEntity>
     {
         private CustomExpressionMeta<TEntity> customExpressionMeta = new();
 
@@ -24,7 +31,16 @@ namespace BenchMark
             customExpressionMeta.decimalFuncMap = Register<decimal>(properties);
         }
 
-        public Dictionary<string, Action<TEntity, TType>> Register<TType>(PropertyInfo[] propertyInfos)
+        public void Set(TEntity instance, string propertyName, string value)
+            => customExpressionMeta.stringFuncMap[propertyName](instance, value);
+
+        public void Set(TEntity instance, string propertyName, decimal value)
+            => customExpressionMeta.decimalFuncMap[propertyName](instance, value);
+
+        public void Set(TEntity instance, string propertyName, int value)
+            => customExpressionMeta.intFuncMap[propertyName](instance, value);
+
+        private Dictionary<string, Action<TEntity, TType>> Register<TType>(PropertyInfo[] propertyInfos)
         {
             var entityType = typeof(TEntity);
             var propType = typeof(TType);
@@ -44,95 +60,5 @@ namespace BenchMark
                 });
         }
 
-        public void Set(TEntity instance, string propertyName, string value)
-        {
-            customExpressionMeta.stringFuncMap[propertyName](instance, value);
-        }
-
-        public void Set(TEntity instance, string propertyName, decimal value)
-        {
-            customExpressionMeta.decimalFuncMap[propertyName](instance, value);
-        }
-
-        public void Set(TEntity instance, string propertyName, int value)
-        {
-            customExpressionMeta.intFuncMap[propertyName](instance, value);
-        }
-
-
-        /*
-        private void RegisterStringProperties()
-        {
-            // Assuming all string properties have a setter that takes a string argument
-            var type = typeof(T);
-            var instanceParam = Expression.Parameter(type);
-            var argumentParam = Expression.Parameter(typeof(string));
-
-            stringFuncMap = type.GetProperties().Where(x => x.PropertyType == typeof(string))
-                               .ToDictionary(x => x.Name, x =>
-                               {
-                                   var expression = Expression.Lambda<Action<T, string>>(
-                               Expression.Call(instanceParam, x.GetSetMethod(), argumentParam),
-                               instanceParam, argumentParam
-                             ).Compile();
-
-                                   return expression;
-                               });
-        }
-
-        private void RegisterIntProperties()
-        {
-            // Assuming all int properties have a setter that takes an int argument
-            var type = typeof(T);
-            var instanceParam = Expression.Parameter(type);
-            var argumentParam = Expression.Parameter(typeof(int));
-
-            intFuncMap = type.GetProperties().Where(x => x.PropertyType == typeof(int))
-                               .ToDictionary(x => x.Name, x =>
-                               {
-                                   var expression = Expression.Lambda<Action<T, int>>(
-                               Expression.Call(instanceParam, x.GetSetMethod(), argumentParam),
-                               instanceParam, argumentParam
-                             ).Compile();
-
-                                   return expression;
-                               });
-        }
-
-        private void RegisterDoubleProperties()
-        {
-            // Assuming all decimal properties have a setter that takes a decimal argument
-            var type = typeof(T);
-            var instanceParam = Expression.Parameter(type);
-            var argumentParam = Expression.Parameter(typeof(decimal));
-
-            decimalFuncMap = type.GetProperties().Where(x => x.PropertyType == typeof(decimal))
-                               .ToDictionary(x => x.Name, x =>
-                               {
-                                   var expression = Expression.Lambda<Action<T, decimal>>(
-                               Expression.Call(instanceParam, x.GetSetMethod(), argumentParam),
-                               instanceParam, argumentParam
-                             ).Compile();
-
-                                   return expression;
-                               });
-        }
-
-        
-        public void Set(T instance, string propertyName, string value)
-        {
-            stringFuncMap[propertyName](instance, value);
-        }
-
-
-        public void Set(T instance, string propertyName, int value)
-        {
-            intFuncMap[propertyName](instance, value);
-        }
-
-        public void Set(T instance, string propertyName, decimal value)
-        {
-            decimalFuncMap[propertyName](instance, value);
-        }*/
     }
 }
